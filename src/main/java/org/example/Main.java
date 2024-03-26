7fa964e7d386d11f0c52e35cfcf5126ff21eb45e
Mateusz Kulec <101333426+MateuszRL96@users.noreply.github.com>
2024-03-26 16:51:51 +0100

    robie dalej ucze sie sdsdds

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,24 @@
 package org.example;
 
 import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.videoio.VideoCapture;
 
 
 public class Main {
-    public static void main(String[] args) throws Exception{
-        //System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+    public static void main(String[] args) throws Exception {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 
+        WindowImage fw = new WindowImage("Webcam Capture");
 
+        VideoCapture capture = new VideoCapture(0);
+
+        Mat frame = new Mat();
+
+        while (true) {
+            if (capture.read(frame)) {
+                fw.setImage(frame);
+            }
+        }
     }
 }
--- src/main/java/org/example/WindowImage.java
+++ src/main/java/org/example/WindowImage.java
@@ -1,13 +1,40 @@
 package org.example;
 
+import org.opencv.core.Mat;
+
 import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
 
 public class WindowImage extends JFrame {
     JLabel labelImage;
+    private BufferedImage Mat2BufferedImage(Mat matrix){
+        int type = BufferedImage.TYPE_BYTE_GRAY;
+        if(matrix.channels() > 1){
+            type = BufferedImage.TYPE_3BYTE_BGR;
+        }
+        int bufferSize = matrix.channels() * matrix.cols() * matrix.rows();
+        byte[] buffer = new byte[bufferSize];
+        matrix.get(0, 0, buffer);
+        BufferedImage image = new BufferedImage(matrix.cols(), matrix.rows(), type);
+        final byte[] targetPixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
+        System.arraycopy(buffer, 0, targetPixels, 0, buffer.length);
+        return image;
+    }
     public WindowImage(String title) {
         super(title);
         labelImage = new JLabel();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        add(labelImage);
+        setVisible(true);
+    }
 
+    public void setImage(Mat matImage){
+        Image image = Mat2BufferedImage(matImage);
+        ImageIcon imgIcon = new ImageIcon(image);
+        setSize(matImage.cols(), matImage.rows());
+        labelImage.setIcon(imgIcon);
+        labelImage.repaint();
     }
 }
