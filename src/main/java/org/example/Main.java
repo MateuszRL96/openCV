c7e656b33a1bf2068c972e24449f30839ae81309
Mateusz Kulec <101333426+MateuszRL96@users.noreply.github.com>
2024-03-28 09:47:45 +0100

    kolejny part

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,11 +8,14 @@ import org.opencv.videoio.VideoCapture;
 public class Main {
     public static void main(String[] args) throws Exception {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-
         WindowImage fw = new WindowImage("Webcam Capture");
-
         VideoCapture capture = new VideoCapture(0);
 
+        if(!capture.isOpened()){
+            System.out.println("Unable to open the camera");
+            capture.release();
+            System.exit(0);
+        }
         Mat frame = new Mat();
 
         while (true) {
